e911a21cdced029ff379919843902046aefc1a47
Artem Dev <artem.vdovyk@gmail.com>
2021-10-09 17:21:37 +0300

    Dinamize students list

--- students/views.py
+++ students/views.py
@@ -4,7 +4,24 @@ from django.http import HttpResponse
 
 # Views for Students
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name': 'Дмитро',
+         'last_name': 'Літвінов',
+         'ticket': 2123,
+         'image': 'img/Dmytro.jpeg'},
+        {'id': 2,
+         'first_name': 'Віталій',
+         'last_name': 'Подоба',
+         'ticket': 254,
+         'image': 'img/Vitaliy.png'},
+        {'id': 3,
+         'first_name': 'Андрій',
+         'last_name': 'Корост',
+         'ticket': 5332,
+         'image': 'img/Andrew.jpg'},
+    )
+    return render(request, 'students/students_list.html', {'students': students})
 
 
 def students_add(request):
