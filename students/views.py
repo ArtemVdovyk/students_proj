330e1b89fb1cd659778b1dfc3323b69b29a754f8
Artem Dev <artem.vdovyk@gmail.com>
2021-10-23 17:48:19 +0300

    Context processor

--- students/views.py
+++ students/views.py
@@ -38,7 +38,16 @@ def students_delete(request, sid):
 
 # Views for Groups
 def groups_list(request):
-    return HttpResponse('<h1>Groups Listing</h1>')
+    groups = (
+        {'id': 1,
+         'name': 'Мтм-21',
+         'leader': {'id': 1, 'name': 'Літвінов Дмитро'}},
+        {'id': 2,
+         'name': 'Мтм-22',
+         'leader': {'id': 2, 'name': 'Віталій Подоба'}},
+    )
+
+    return render(request, 'students/groups_list.html', {'groups': groups})
 
 
 def groups_add(request):
