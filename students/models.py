a3c2cd080e3d16f5179da53a10209d9e0ba04e5b
ArtemVdovyk <artem.vdovyk@gmail.com>
2022-01-11 23:52:33 +0200

    Added model Group and migrate from sqlite to PostgreSQL

--- students/models.py
+++ students/models.py
@@ -4,41 +4,40 @@ from django.db import models
 class Student(models.Model):
     """Student Model"""
 
-    first_name = models.CharField(
-        max_length=256,
-        blank=False,
-        verbose_name="Ім'я"
-    )
-    last_name = models.CharField(
-        max_length=256,
-        blank=False,
-        verbose_name="Прізвище"
-    )
-    middle_name = models.CharField(
-        max_length=256,
-        blank=True,
-        verbose_name="По-батькові",
-        default=""
-    )
-    birthday = models.DateField(
-        blank=False,
-        verbose_name="Дата народження",
-        null=True
-    )
-    photo = models.ImageField(
-        blank=True,
-        verbose_name="Фото",
-        null=True
-    )
-    ticket = models.CharField(
-        max_length=256,
-        blank=False,
-        verbose_name="Білет"
-    )
-    notes = models.TextField(
-        blank=True,
-        verbose_name="Додаткові нотатки"
-    )
+    first_name = models.CharField(max_length=256,
+                                  blank=False,
+                                  verbose_name="Ім'я"
+                                  )
+    last_name = models.CharField(max_length=256,
+                                 blank=False,
+                                 verbose_name="Прізвище"
+                                 )
+    middle_name = models.CharField(max_length=256,
+                                   blank=True,
+                                   verbose_name="По-батькові",
+                                   default=""
+                                   )
+    birthday = models.DateField(blank=False,
+                                verbose_name="Дата народження",
+                                null=True
+                                )
+    photo = models.ImageField(blank=True,
+                              verbose_name="Фото",
+                              null=True
+                              )
+    ticket = models.CharField(max_length=256,
+                              blank=False,
+                              verbose_name="Білет"
+                              )
+    notes = models.TextField(blank=True,
+                             verbose_name="Додаткові нотатки"
+                             )
+    student_group = models.ForeignKey('Group',
+                                      verbose_name="Група",
+                                      blank=False,
+                                      null=True,
+                                      on_delete=models.PROTECT
+                                      )
 
     def __str__(self):
         """Return the first_name plus the last_name, with a space in between"""
@@ -48,3 +47,30 @@ class Student(models.Model):
     class Meta:
         verbose_name = "Студент"
         verbose_name_plural = "Студенти"
+
+
+class Group(models.Model):
+    """Group Model"""
+
+    title = models.CharField(max_length=256,
+                             verbose_name="Назва"
+                             )
+    leader = models.OneToOneField('Student',
+                                  verbose_name="Староста",
+                                  blank=True,
+                                  null=True,
+                                  on_delete=models.SET_NULL
+                                  )
+    notes = models.TextField(blank=True,
+                             verbose_name="Додаткові нотатки"
+                             )
+
+    class Meta:
+        verbose_name = "Група"
+        verbose_name_plural = "Групи"
+
+    def __str__(self):
+        if self.leader:
+            return f"{self.title} ({self.leader.first_name} {self.leader.last_name})"
+        else:
+            return f"{self.title}"
--- students_proj/settings.py
+++ students_proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from .db import DATABASES
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,12 +76,7 @@ WSGI_APPLICATION = 'students_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = DATABASES
 
 
 # Password validation
