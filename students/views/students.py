fca0db630696e51d3cfae26854317ce28ea4d03d
ArtemVdovyk <artem.vdovyk@gmail.com>
2022-01-19 09:07:35 +0200

    Added validate student. Step 1

--- students/views/students.py
+++ students/views/students.py
@@ -40,19 +40,51 @@ def students_add(request):
     if request.method == "POST":
         # was form add button clicked?
         if request.POST.get('add_button') is not None:
-            # TODO: validate input from user
+            # errors collection
             errors = {}
+            # validate student data will go here
+            data = {'middle_name': request.POST.get('middle_name'),
+                    'notes': request.POST.get('notes')}
 
+            # validate user input
+            first_name = request.POST.get('first_name', '').strip()
+            if not first_name:
+                errors['first_name'] = "Ім'я є обов'язковим"
+            else:
+                data['first_name'] = first_name
+
+            last_name = request.POST.get('last_name', '').strip()
+            if not last_name:
+                errors['last_name'] = "Прізвище є обов'язковим"
+            else:
+                data['last_name'] = last_name
+
+            birthday = request.POST.get('birthday', '').strip()
+            if not birthday:
+                errors['birthday'] = "Дата народження є обов'язковою"
+            else:
+                data['birthday'] = birthday
+
+            ticket = request.POST.get('ticket', '').strip()
+            if not ticket:
+                errors['ticket'] = "Номер білету є обов'язковим"
+            else:
+                data['ticket'] = ticket
+
+            student_group = request.POST.get('student_group', '').strip()
+            if not student_group:
+                errors['student_group'] = "Оберіть групу для студента"
+            else:
+                data['student_group'] = Group.objects.get(pk=student_group)
+
+            photo = request.FILES.get('photo')
+            if photo:
+                data['photo'] = photo
+
+            # save student
             if not errors:
-            # create student object
-                student = Student(first_name=request.POST['first_name'],
-                                  last_name=request.POST['last_name'],
-                                  middle_name=request.POST['middle_name'],
-                                  birthday=request.POST['birthday'],
-                                  ticket=request.POST['ticket'],
-                                  student_group=Group.objects.get(pk=request.POST['student_group']),
-                                  photo=request.FILES['photo'],
-                                  )
+                # create student object
+                student = Student(**data)
                 # save it to database
                 student.save()
                 # redirect user to students list
