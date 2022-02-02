26341cf38ef75e35afc260b2593ab02c7a111678
ArtemVdovyk <artem.vdovyk@gmail.com>
2022-02-02 19:04:55 +0200

    Added CrispyForm Styles to Student Edit Form

--- students/views/students.py
+++ students/views/students.py
@@ -1,9 +1,13 @@
+from crispy_forms.bootstrap import FormActions
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import render
+from django.forms import ModelForm
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse
-from datetime import datetime
 from django.views.generic import UpdateView
+from datetime import datetime
 
 from students.models.students import Student
 from students.models.groups import Group
@@ -114,10 +118,38 @@ def students_add(request):
         return render(request, 'students/students_add.html', {'groups': Group.objects.all().order_by('title')})
 
 
+class StudentUpdateForm(ModelForm):
+
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+
+        # set form tag attributes
+        self.helper.form_action = reverse('students_edit', kwargs={'pk': kwargs['instance'].id})
+        self.helper.form_method = 'POST'
+        self.helper.form_class = 'form-horizontal'
+
+        # set form field properties
+        self.helper.help_text_inline = True
+        self.helper.html5_required = True
+        self.helper.label_class = 'col-sm-2 col-form-label'
+        self.helper.field_class = 'col-sm-10'
+
+        # add buttons
+        self.helper.layout.append(FormActions(Submit('add_button', 'Зберегти'),
+                                              Submit('cancel_button', 'Скасувати', css_class='btn-danger')
+                                              ))
+
+
 class StudentUpdateView(UpdateView):
     model = Student
-    fields = '__all__'
     template_name = 'students/students_edit.html'
+    form_class = StudentUpdateForm
 
     def get_success_url(self):
         return f"{reverse('home')}?status_message=Студента успішно збережено!"
@@ -127,13 +159,6 @@ class StudentUpdateView(UpdateView):
             return HttpResponseRedirect(f"{reverse('home')}?status_message=Редагування студента відмінено!")
         else:
             return super().post(request, *args, **kwargs)
-
-    
-
-
-
-def students_edit(request, sid):
-    return HttpResponse('<h1>Edit Student %s</h1>' % sid)
 
 
 def students_delete(request, sid):
