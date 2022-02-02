e4056c6d6b25aa99c2fac6a708c07f9c00ec620c
ArtemVdovyk <artem.vdovyk@gmail.com>
2022-02-02 22:32:10 +0200

    Added validator for field Groups

--- students/admin.py
+++ students/admin.py
@@ -7,6 +7,19 @@ from .models.students import Student
 from .models.groups import Group
 
 
+class StudentFormAdmin(ModelForm):
+
+    def clean_student_group(self):
+        """Check if student is leader in any group.
+        If yes, then ensure it's the same as selected group."""
+        # get group where current student is a leader
+        group = Group.objects.filter(leader=self.instance).first()
+        if group and (self.cleaned_data['student_group'] != group):
+            raise ValidationError('Cтудент є старостою іншої групи.', code='invalid')
+
+        return self.cleaned_data['student_group']
+
+
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['last_name', 'first_name', 'ticket', 'student_group']
     list_display_links = ['last_name', 'first_name']
@@ -15,6 +28,7 @@ class StudentAdmin(admin.ModelAdmin):
     list_filter = ['student_group']
     list_per_page = 10
     search_fields = ['last_name', 'first_name', 'middle_name', 'ticket', 'notes']
+    form = StudentFormAdmin
 
     def view_on_site(self, obj):
         return reverse('students_edit', kwargs={'pk': obj.id})
