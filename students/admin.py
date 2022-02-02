1e453210bbf26250ecc83f256fbf7366e8f55351
ArtemVdovyk <artem.vdovyk@gmail.com>
2022-02-02 20:20:13 +0200

    Custom Admin part. Step 1

--- students/admin.py
+++ students/admin.py
@@ -1,6 +1,38 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm
+from django.urls import reverse
+
 from .models.students import Student
 from .models.groups import Group
 
-admin.site.register(Student)
-admin.site.register(Group)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name', 'ticket', 'student_group']
+    list_display_links = ['last_name', 'first_name']
+    list_editable = ['student_group']
+    ordering = ['last_name']
+    list_filter = ['student_group']
+    list_per_page = 10
+    search_fields = ['last_name', 'first_name', 'middle_name', 'ticket', 'notes']
+
+    def view_on_site(self, obj):
+        return reverse('students_edit', kwargs={'pk': obj.id})
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['title', 'leader']
+    list_display_links = ['title']
+    list_editable = ['leader']
+    ordering = ['title']
+    list_filter = ['title']
+    list_per_page = 10
+    search_fields = ['title', 'leader', 'notes']
+
+    def view_on_site(self, obj):
+        return reverse('groups_edit', kwargs={'pk': obj.id})
+
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Group, GroupAdmin)
+
